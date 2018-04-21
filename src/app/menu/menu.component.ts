fa6df9333351c1c63040c61c27bb3b24da88bd39
Lexa <ying3268na@gmail.com>
2018-04-22 02:45:59 +1200

    Angular_component_Part1

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Dish } from '../shared/dish';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  dishes: Dish[] = [
+    {
+      name: 'Uthappizza',
+      image: '/assets/images/uthappizza.png',
+      category: 'mains',
+      label: 'Hot',
+      price: '4.99',
+      description: 'A unique combination of Indian Uthappam (pancake)  '},
+   {
+      name: 'Zucchipakoda',
+      image: '/assets/images/zucchipakoda.png',
+      category: 'appetizer',
+      label: '',
+      price: '1.99',
+      description: 'Deep fried Zucchini coated with mildly spiced ' },
+   {
+      name: 'Vadonut',
+      image: '/assets/images/vadonut.png',
+      category: 'appetizer',
+      label: 'New',
+      price: '1.99',
+      description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
+   {
+      name: 'ElaiCheese Cake',
+      image: '/assets/images/elaicheesecake.png',
+      category: 'dessert',
+      label: '',
+      price: '2.99',
+      description: 'A delectable, semi-sweet New York Style Cheese Cake' }
+   ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
