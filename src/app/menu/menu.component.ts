be30fc4c610e47d25d0c79ba4b198e07f66fb005
Lexa <ying3268na@gmail.com>
2018-04-22 04:16:37 +1200

    Angular_component_Part2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,6 +2,38 @@ import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish';
 
+const DISHES: Dish[] = [
+  {
+    name: 'Uthappizza',
+    image: '/assets/images/uthappizza.png',
+    category: 'mains',
+    label: 'Hot',
+    price: '4.99',
+    description: 'A unique combination of Indian Uthappam (pancake)  '},
+ {
+    name: 'Zucchipakoda',
+    image: '/assets/images/zucchipakoda.png',
+    category: 'appetizer',
+    label: '',
+    price: '1.99',
+    description: 'Deep fried Zucchini coated with mildly spiced ' },
+ {
+    name: 'Vadonut',
+    image: '/assets/images/vadonut.png',
+    category: 'appetizer',
+    label: 'New',
+    price: '1.99',
+    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
+ {
+    name: 'ElaiCheese Cake',
+    image: '/assets/images/elaicheesecake.png',
+    category: 'dessert',
+    label: '',
+    price: '2.99',
+    description: 'A delectable, semi-sweet New York Style Cheese Cake' }
+ ];
+
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -9,36 +41,8 @@ import { Dish } from '../shared/dish';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = [
-    {
-      name: 'Uthappizza',
-      image: '/assets/images/uthappizza.png',
-      category: 'mains',
-      label: 'Hot',
-      price: '4.99',
-      description: 'A unique combination of Indian Uthappam (pancake)  '},
-   {
-      name: 'Zucchipakoda',
-      image: '/assets/images/zucchipakoda.png',
-      category: 'appetizer',
-      label: '',
-      price: '1.99',
-      description: 'Deep fried Zucchini coated with mildly spiced ' },
-   {
-      name: 'Vadonut',
-      image: '/assets/images/vadonut.png',
-      category: 'appetizer',
-      label: 'New',
-      price: '1.99',
-      description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-   {
-      name: 'ElaiCheese Cake',
-      image: '/assets/images/elaicheesecake.png',
-      category: 'dessert',
-      label: '',
-      price: '2.99',
-      description: 'A delectable, semi-sweet New York Style Cheese Cake' }
-   ];
+  dishes: Dish[] = DISHES;
+  selectedDish: Dish = DISHES[0];
 
   constructor() { }
 
